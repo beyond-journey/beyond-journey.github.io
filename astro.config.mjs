f0f21fc1197f8e393f2b94008d4493c90546a881
Max Graw <max@graw-online.de>
2022-08-21 13:32:38 +0200

    created project

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+// https://docs.astro.build/de/reference/configuration-reference/
+export default defineConfig({
+    site: 'https://bejond-journey.github.io'
+});
